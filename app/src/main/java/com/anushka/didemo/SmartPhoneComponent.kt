8bb3cb16b9fce54f3f9935fb86fe3a262d4385a1
Gretel Hilbing <ghilbing@gmail.com>
2021-09-30 09:23:50 -0700

    Field injection

--- app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
+++ app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
@@ -4,6 +4,7 @@ import dagger.Component
 
 @Component(modules = [MemoryCardModule::class, NCBatteryModule::class])
 interface SmartPhoneComponent {
-    fun getSmartPhone(): SmartPhone
+    //fun getSmartPhone(): SmartPhone
+    fun inject(mainActivity: MainActivity)
 
 }
